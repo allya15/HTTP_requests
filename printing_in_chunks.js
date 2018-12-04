b10fab259482c48b1424cdb4594a78b623cc0fe8
Ally <allison.avis@gmail.com>
2018-12-04 05:52:22 +0000

    printing in chunks function working

--- printing_in_chunks.js
+++ printing_in_chunks.js
@@ -7,4 +7,22 @@ var requestOptions = {
 
 function getAndPrintHTMLChunks () {
 
+  https.get(requestOptions, function(response) {
+
+    if (response.statusCode !== 200) {
+      console.log("Error loading page: ", response.statuscode);
+    }
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      console.log('Chunk Received. Content: \n' + data + "\n");
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
 }
+
+getAndPrintHTMLChunks();
